130b748a6cbba5b21535443567f8a01eb86b19a2
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:51:23 -0300

    Teste do uso de variável com o tipo de dados String em Java.

---
+++ src/TesteVariavelString.java
@@ -0,0 +1,8 @@
+public class TesteVariavelString {
+
+    public static void main(String[] args) {
+        String comentario = "O tipo String aceita textos, números e outros caracteres como por exemplo:\nabc\n123\n%$#@";
+
+        System.out.printf("Comentário:\n\n%s", comentario);
+    }
+}
